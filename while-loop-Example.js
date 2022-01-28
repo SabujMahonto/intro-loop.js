00ac32baa95e211a2ac73bf9647b44453bf3e929
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 23:35:57 +0600

    while consept done

---
+++ while-loop-Example.js
@@ -0,0 +1,22 @@
+var number = 0;  // loop variable 
+
+while(number <= 10){
+    console.log(number);
+    console.log('a');
+    // number = number + 1;
+    // number += 1;
+    number++
+}
+
+// Ever number  jor shonkha
+var number = 1;
+while(number <=20){
+    console.log(number);
+    number = number + 1;
+}
+// odd number  jor shonkha
+var number = 1;
+while(number <=20){
+    console.log(number);
+    number = number + 2;
+}
