ab4ae19f03f8c17e11317c92ef511a01d337d762
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 23:11:27 +0600

    while loop concept done

---
+++ while-loop.js
@@ -0,0 +1,9 @@
+var rostGiven = 0;
+
+while(rostGiven < 7 ){
+    console.log("rost give me please!!");
+    // rostGiven = rostGiven + 1;
+    // rostGiven += 1;
+    rostGiven++;
+    console.log(rostGiven);
+}
